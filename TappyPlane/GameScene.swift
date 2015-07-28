cebadc035ec8aef68bf430704bb66fdf6fade99b
Atikur Rahman <atik.bd08@gmail.com>
2015-07-28 23:30:51 +0600

    create scrolling background layer

--- TappyPlane/GameScene.swift
+++ TappyPlane/GameScene.swift
@@ -13,13 +13,34 @@ class GameScene: SKScene {
     var world: SKNode!
     var player: Plane!
     
+    let minFPS: NSTimeInterval = 10/60
+    
+    var background: ScrollingLayer!
+    var lastCallTime: NSTimeInterval = 0
+    
     override init(size: CGSize) {
         super.init(size: size)
+        
+        let graphics = SKTextureAtlas(named: "Graphics")
         
         self.physicsWorld.gravity = CGVectorMake(0, -5.5)
         
         world = SKNode()
         addChild(world)
+        
+        var backgroundTiles = [SKSpriteNode]()
+        
+        // setup background
+        for i in 0..<3 {
+            let tile = SKSpriteNode(texture: graphics.textureNamed("background"))
+            backgroundTiles.append(tile)
+        }
+        
+        background = ScrollingLayer(tileSpriteNodes: backgroundTiles)
+        background.position = CGPointZero
+        background.horizontalScrollingSpeed = -60
+        background.scrolling = true
+        world.addChild(background)
         
         player = Plane()
         player.position = CGPointMake(self.size.width/2, self.size.height/2)
@@ -43,7 +64,16 @@ class GameScene: SKScene {
     }
     
     override func update(currentTime: NSTimeInterval) {
+        var timeElapsed = currentTime - lastCallTime
+        lastCallTime = currentTime
+        
+        // in case it takes too long (probably first time when scene loads)
+        if timeElapsed > minFPS {
+            timeElapsed = minFPS
+        }
+        
         player.update()
+        background.update(timeElapsed)
     }
 
     required init?(coder aDecoder: NSCoder) {
